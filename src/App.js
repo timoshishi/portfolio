23e39551205a957261c1a1b2dbe95e7af53e0528
timoshishi <timfrrst@gmail.com>
2021-03-19 15:42:43 -0400

    Update structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';
-import { lightTheme, darkTheme } from './styles/Themes';
-
+import GlobalStyles from './components/GlobalStyles';
+import { lightTheme, darkTheme } from './styles/themes';
 import useDarkMode from './hooks/useDarkMode';
+
 const Wrapper = styled.h1`
   color: ${({ theme }) => theme.color};
   background: ${({ theme }) => theme.background};
@@ -12,6 +12,7 @@ const Wrapper = styled.h1`
 const App = () => {
   const [isDarkTheme, themeToggler] = useDarkMode();
   const themeMode = isDarkTheme ? darkTheme : lightTheme;
+
   return (
     <React.Fragment>
       <ThemeProvider theme={themeMode}>
