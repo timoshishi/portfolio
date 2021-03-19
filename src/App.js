58e4e729f3c4d3698ca69a47155092c7cf4ab670
timoshishi <timfrrst@gmail.com>
2021-03-19 12:11:27 -0400

    Add packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div className='App'>HELLO WORLD</div>;
-}
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <div className='App'>HELLO WORLD</div>
+    </React.Fragment>
+  );
+};
 
 export default App;
