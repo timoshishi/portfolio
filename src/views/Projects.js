e4395402f8ee02abbfda5c8c77f5e58420fb2f5f
timoshishi <timfrrst@gmail.com>
2021-04-03 12:12:45 -0400

    Add routes

---
+++ src/views/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Projects = (props) => {
+  return <h1>Hello Projects</h1>;
+};
+
+Projects.propTypes = {};
+
+export default Projects;
