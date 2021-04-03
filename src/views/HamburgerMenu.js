2ca421e380af5ac02db0321643e4c8a6d628b0b6
timoshishi <timfrrst@gmail.com>
2021-04-03 12:28:55 -0400

    Close menu on link click

--- src/views/HamburgerMenu.js
+++ src/views/HamburgerMenu.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { scaleRotate as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom';
 const HamburgerMenu = ({ children }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div id='App'>
-      <Menu id='side-bar' pageWrapId={'page-wrap'} outerContainerId={'App'}>
-        <Link to='/' className='menu-item'>
+      <Menu
+        id='side-bar'
+        pageWrapId={'page-wrap'}
+        outerContainerId={'App'}
+        isOpen={menuOpen}
+        onStateChange={(state) => {
+          setMenuOpen(state.menuOpen);
+        }}>
+        <Link to='/' className='menu-item' onClick={() => setMenuOpen(false)}>
           Home
         </Link>
-        <Link to='/projects' className='menu-item'>
+        <Link
+          to='/projects'
+          className='menu-item'
+          onClick={() => setMenuOpen(false)}>
           Projects
         </Link>
-        <Link to='/about' className='menu-item'>
+        <Link
+          to='/about'
+          className='menu-item'
+          onClick={() => setMenuOpen(false)}>
           About
         </Link>
-        <a className='menu-item' href='#contact'>
+        <a
+          className='menu-item'
+          href='#contact'
+          onClick={() => setMenuOpen(false)}>
           Contact
         </a>
       </Menu>
