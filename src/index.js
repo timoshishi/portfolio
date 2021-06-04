831ca91da1cace544a936c5950ab15d5f644da41
timoshishi <timfrrst@gmail.com>
2021-06-04 11:57:43 -0400

    Add analytics

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/global.css';
-// import reportWebVitals from './reportWebVitals';
+// import { reportWebVitals } from './reportWebVitals';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-198621727-1', {
+  debug: process.env.NODE_ENV === 'development' ? true : false,
+  titleCase: false,
+  gaOptions: {
+    userId: 123,
+  },
+}); // add your tracking id here.
+console.log('process', process.env);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
